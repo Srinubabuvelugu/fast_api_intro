f1e7172b1e07a0ec3c40c959494ca247551aa0a5
Srinubabuvelugu <121814804+Srinubabuvelugu@users.noreply.github.com>
2024-10-30 11:20:24 +0530

    Add files via upload

---
+++ sub_app/routers/__init__.py
@@ -0,0 +1,2 @@
+from .items import router as itemṣ_router
+from .users import router as users_router
---
+++ sub_app/routers/users.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/users/", tags = ["users"])
+async def read_users():
+    return [{"username":"Babu"},{"username":"Siva"}]
+
+@router.get("/users/me",tags=["users"])
+async def read_user_me():
+    return {"username":"currentuser"}
+
+
+@router.get("/users/{username}",tags=["users"])
+async def read_user(username: str):
+    return {"username":username}
